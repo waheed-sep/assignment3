b2fde3d7bfd3a40698a0d609812de3d3b2eae3da
Muhammad Waheed Khan <waheed.sep@gmail.com>
2023-12-01 20:03:09 +0500

    unit test modified

--- tests/test_home.py
+++ tests/test_home.py
@@ -5,9 +5,9 @@ class TestApp(unittest.TestCase):
         response = 200
         assert response == 200
 
-    def test_home_not_200(self):
-        response = 400
-        assert response != 200
+    # def test_home_not_200(self):
+    #     response = 400
+    #     assert response != 200
 
 if __name__ == "__main__":
     unittest.main()
